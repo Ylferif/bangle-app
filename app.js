b280f835639ce6e508f96a2b2169cf6306130c33
Peter <peter@aml-analytics.com>
2022-07-21 13:54:50 +0100

    revert

--- app.js
+++
@@ -1,9 +0,0 @@
-import "https://www.puck-js.com/puck.js";
-
-function writeFile(name, content) {
-  Puck.write(`require('Storage').write('${name}',atob("${btoa(content)}"));\n`);
-}
-
-document.getElementById("test").addEventListener("click", () => {
-  writeFile("example.json", "hello world!");
-});
