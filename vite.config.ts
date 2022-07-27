f71e77357bc496e67df75d36747798bb2940279a
Peter <peter@aml-analytics.com>
2022-07-27 10:48:15 +0100

    C

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,25 @@ export default defineConfig({
     svelte(),
     VitePWA({
       registerType: "autoUpdate",
+      includeAssets: [
+        "favicon.ico",
+        "apple-touch-icon.png",
+        "android-chrome-512x512.png",
+        "vite.svg",
+      ],
+      manifest: {
+        name: "My Awesome App",
+        short_name: "Bangle",
+        description: "My Awesome App description",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
       devOptions: {
         enabled: true,
       },
