c72046a3f2d08cb70c2733acd24ac00e71b52e99
Peter <peter@aml-analytics.com>
2022-07-27 10:13:08 +0100

    PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     outDir: "docs",
   },
-  plugins: [svelte()],
+  plugins: [
+    svelte(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
 });
